c3f0e39fbc835db135896f35125446362553764f
Enoxboo <matteo.marquant@ynov.com>
2025-09-15 22:58:39 +0200

    Adding Wind and Earth Dash

--- Scripts/Player/Elements/earth.gd
+++ Scripts/Player/Elements/earth.gd
@@ -7,6 +7,15 @@ func handle_attack(player):
 	earth_player = player
 	player.sprite.modulate = Color("BROWN")
 
+func handle_dash(player):
+	if player.is_on_floor():
+		var initial_dash_force: int = player.dash_force
+		player.dash_force = 1000
+		super.handle_dash(player)
+		player.dash_force = initial_dash_force
+	else:
+		super.handle_dash(player)
+
 func enemy_hit(enemy: Node2D, side: int) -> void:
 	super.enemy_hit(enemy, side)
 	if earth_player.is_on_floor():
--- Scripts/Player/Elements/wind.gd
+++ Scripts/Player/Elements/wind.gd
@@ -7,6 +7,15 @@ func handle_attack(player):
 	wind_player = player
 	player.sprite.modulate = Color("GREEN")
 
+func handle_dash(player):
+	if not player.is_on_floor():
+		var initial_dash_force: int = player.dash_force
+		player.dash_force = 1000
+		super.handle_dash(player)
+		player.dash_force = initial_dash_force
+	else:
+		super.handle_dash(player)
+
 func enemy_hit(enemy: Node2D, side: int) -> void:
 	super.enemy_hit(enemy, side)
 	if not wind_player.is_on_floor():
